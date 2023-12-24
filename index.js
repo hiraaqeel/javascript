e8cb221a064a6157450bbebd6369811173da09b7
hiraaqeel <hk4645953@gmail.com>
2023-12-24 13:03:47 +0500

    grading

---
+++ index.js
@@ -0,0 +1,22 @@
+// Grade point table
+const marks = prompt("Please enter your marks");
+
+// MARKS GREATER THAN 90
+if(marks >= 90){
+    console.log( "got A+ grade");
+}
+
+// MARKS GREATER THAN 85 AND LESS THAN 90
+if(marks >=85 && marks < 90){
+    console.log("got A grade")
+}
+
+// MARKS GREATER THAN 80 AND LESS THAN 85
+if(marks >= 80 && marks < 85 ){
+    console.log("got A- grade")
+}
+
+// MARKS GREATER THAN 75 AND LESS THAN 80
+if(marks >= 75 && marks < 80){
+    console.log("got B+ grade")
+}
